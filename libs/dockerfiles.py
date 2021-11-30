6962c49bbe4eae31627cde0a58f3ddc9f0f48caa
aoimaru <51444995+aoimaru@users.noreply.github.com>
2021-12-01 00:32:26 +0900

    コンテンツpropertyの作成

--- libs/dockerfiles.py
+++ libs/dockerfiles.py
@@ -4,4 +4,9 @@ import re
 class Dockerfile(object):
     def __init__(self, file_path):
         self._contents = dockerfile.parse_file(file_path)
+    
 
+    @property
+    def contents(self):
+        return self._contents
+    
