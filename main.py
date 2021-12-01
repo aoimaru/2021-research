3af37320273f2c71c58f27bd9270a90a90a197ed
aoimaru <51444995+aoimaru@users.noreply.github.com>
2021-12-01 14:34:58 +0900

    Modelクラスの準備完了

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import glob
 import os
 
-# from libs.dockerfiles import Model
+from libs.dockerfiles import Model
 from libs.primitives import Primitive
 from libs.structures import Structure
  
@@ -13,22 +13,9 @@ def test():
     file_paths = [comp for comp in glob.glob(PYTHON_PROJECT, recursive=True) if os.path.isfile(comp) if comp.endswith("Dockerfile")]
     for file_path in file_paths:
         model = Model(file_path)
-        runs = model.runs
-        for run in runs:
-            cnt = []
-            for shells in run:
-                while shells:
-                    word = shells.pop(0)
-                    if not word:
-                        continue
-                    if word == "NL":
-                        continue
-                    if word == "SPACE":
-                        continue
-                    cnt.append(word)
-            print()
-            print(cnt)
-
+        primitives = model.primitives
+        for primitive in primitives:
+            print(primitive)
 
 
 
