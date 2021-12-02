88fed803303b8a4d33e8ea9d05a9a9c1430833a0
aoimaru <51444995+aoimaru@users.noreply.github.com>
2021-12-02 15:31:33 +0900

    一旦コミット

--- libs/dock2vecs.py
+++ libs/dock2vecs.py
@@ -26,6 +26,31 @@ class D2V():
             training_docs.append(training_doc)
         model = Doc2Vec(documents=training_docs, min_count=1, dm=0)
         return model, hash_dict, hash_key
+    
+
+
+    @staticmethod
+    def execute_sec(tokens):
+        hash_dict = {}
+        hash_key = []
+        training_docs = []
+        for token in tokens:
+            # print("token:", token)
+            rs = " ".join(token)
+            hash_object = hashlib.sha256(rs.encode()).hexdigest()
+            hash_key.append(hash_object)
+            if not hash_object in hash_dict:
+                hash_dict[hash_object] = []
+            hash_dict[hash_object].append(token)
+            training_doc = TaggedDocument(
+                words=token, 
+                tags=[hash_object]
+            )
+
+            training_docs.append(training_doc)
+        model = Doc2Vec(documents=training_docs, min_count=1, dm=0)
+        return model, hash_dict, hash_key
+
 
 
         
