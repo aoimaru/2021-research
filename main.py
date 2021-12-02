55be7f907df9cfdd724a9c29ca6f28789ed646ea
aoimaru <51444995+aoimaru@users.noreply.github.com>
2021-12-02 14:15:36 +0900

    一旦コミット

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ BINNACLE_PROJECT = "./binnacle-icse2020/**"
 def test():
     # df = Dockerfile(TEST_PATH)
     file_paths = [comp for comp in glob.glob(GOLANG_PROJECT, recursive=True) if os.path.isfile(comp) if comp.endswith("Dockerfile")]
-    for file_path in file_paths[:1]:
+    for file_path in file_paths:
         print()
         primitive = Primitive(file_path)
         # print(primitive.data)
@@ -46,7 +46,7 @@ def test():
 
 def test_2():
     file_paths = [comp for comp in glob.glob(PYTHON_PROJECT, recursive=True) if os.path.isfile(comp) if comp.endswith("Dockerfile")]
-    for file_path in file_paths:
+    for file_path in file_paths[:1]:
         print()
         model = Model(file_path)
         primitives = model.primitives
@@ -55,11 +55,10 @@ def test_2():
 
 
 
-
 def main():
 
-    test()
-    # test_2()
+    # test()
+    test_2()
 
 if __name__ == "__main__":
     main()
