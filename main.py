c56f8bf397c37177b628873fffd1a954c1639caa
aoimaru <51444995+aoimaru@users.noreply.github.com>
2021-12-03 02:31:15 +0900

    準備

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ BINNACLE_PROJECT = "./binnacle-icse2020/**"
 
 def test():
     # df = Dockerfile(TEST_PATH)
-    file_paths = [comp for comp in glob.glob(OTHERS_PROJECT, recursive=True) if os.path.isfile(comp) if comp.endswith("Dockerfile")]
+    file_paths = [comp for comp in glob.glob(PYTHON_PROJECT, recursive=True) if os.path.isfile(comp) if comp.endswith("Dockerfile")]
     for file_path in file_paths:
         print()
         primitive = Primitive(file_path)
