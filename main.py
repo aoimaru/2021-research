d9001accd09cc041cc41c1a7e08cc51dee47f4bb
aoimaru <51444995+aoimaru@users.noreply.github.com>
2021-12-06 14:13:30 +0900

    色々削除

--- main.py
+++ main.py
@@ -37,7 +37,6 @@ def test():
                 tokens = Structure.Equal(tokens)
                 print(tokens)
     
-
 def test_4():
     # df = Dockerfile(TEST_PATH)
     file_paths = [comp for comp in glob.glob(BINNACLE_PROJECT, recursive=True) if os.path.isfile(comp) if comp.endswith("Dockerfile")]
@@ -97,21 +96,6 @@ def test_9():
 
     W2V.execute(trainings, "before-after")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 def test_2():
     file_paths = [comp for comp in glob.glob(PYTHON_PROJECT, recursive=True) if os.path.isfile(comp) if comp.endswith("Dockerfile")]
     for file_path in file_paths[:1]:
@@ -147,14 +131,13 @@ def test_3():
         print(key)
     
     
-    print(model.docvecs['7ece24f3b6bffa3df20ce8ada18c4ad430db9932045c9ed199cd84be177f0acf'])
+    print(model.docvecs['83b65817171365b31fe9538d8bd98bca800b35bbbca8f9df22e9f96219dfb0ed'])
 
-    sim_items = model.docvecs.most_similar('7ece24f3b6bffa3df20ce8ada18c4ad430db9932045c9ed199cd84be177f0acf')
+    sim_items = model.docvecs.most_similar('83b65817171365b31fe9538d8bd98bca800b35bbbca8f9df22e9f96219dfb0ed')
 
     for sim_item in sim_items:
         print(sim_item)
         print(hash_dict[sim_item[0]][0], sim_item[1])
-
 
 def test_5():
     path = "./libs/delv/default-2021-12-04 08:58:24.615672.model"
@@ -166,7 +149,6 @@ def test_5():
     for similar_word in similar_words:
 
         print(similar_word)
-
 
 def test_6():
     file_paths = [comp for comp in glob.glob(PYTHON_PROJECT, recursive=True) if os.path.isfile(comp) if comp.endswith("Dockerfile")]
@@ -185,7 +167,6 @@ def test_6():
                 trainings.append(tokens)
     
     W2V.execute(trainings, "naivePython")
-
 
 def test_8():
     file_paths = [comp for comp in glob.glob(PYTHON_PROJECT, recursive=True) if os.path.isfile(comp) if comp.endswith("Dockerfile")]
@@ -214,7 +195,6 @@ def test_7():
 
         print(similar_word)
 
-
 def test_10():
     """
     前後の関係を考慮したモデルの結果
@@ -225,8 +205,6 @@ def test_10():
     for similar_word in similar_words:
 
         print(similar_word)
-
-
 
 def default():
     file_paths = [comp for comp in glob.glob(BINNACLE_PROJECT, recursive=True) if os.path.isfile(comp) if comp.endswith("Dockerfile")]
@@ -292,7 +270,6 @@ def debian_default():
 
     W2V.execute(trainings, "default-test")
 
-
 def debian_default_test():
     path = "libs/delv/default-test-2021-12-06 01:05:49.243366.model"
     # path = "libs/delv/default-test-2021-12-05 01:45:27.835636.model"
@@ -305,7 +282,6 @@ def debian_default_test():
 def add_test():
     path = "libs/delv/default-test-2021-12-06 01:05:49.243366.model"
     model = word2vec.Word2Vec.load(path)
-
 
 def cbow():
     file_paths = [comp for comp in glob.glob(PYTHON_PROJECT, recursive=True) if os.path.isfile(comp) if comp.endswith("Dockerfile")]
@@ -353,10 +329,29 @@ def naive_test():
     for similar_word in similar_words:
         print(similar_word)
 
+def doc2vec_model():
+    file_paths = [comp for comp in glob.glob(DEBIAN_BINNACLE_PROJECT, recursive=True) if os.path.isfile(comp) if comp.endswith("Dockerfile")]
+    trainings = []
+    for file_path in file_paths:
+        # 
+        print()
+        primitive = Primitive(file_path)
+        # print(primitive.data)
+        responses, hash_dict = Structure.toStack(primitive.data)
+        for response in responses:
+            # contents = []
+            for res in response:
+                tokens = hash_dict[res]
+                tokens = Structure.toToken(tokens)
+                tokens = Structure.Equal(tokens)
+                trainings.append(tokens)
+    
+    D2V.execute(trainings, "naiveDebian")
+
 def main():
 
 
-    # test_3()
+    test_3()
 
 
 
@@ -380,7 +375,7 @@ def main():
     # test_10()
     # default_test()
     # debian_default_test()
-    naive_test()
+    # naive_test()
 
 
 
